ec6cd205daeb6d268cd42ff896f0ad4e1da359fd
1b3r1c0 <baconandeggs@pm.me>
2021-02-11 08:56:26 -0500

    cleaned up per pep 8, added comments

--- geojson.py
+++ geojson.py
@@ -10,10 +10,6 @@ SERVICEURL = 'http://py4e-data.dr-chuck.net/json?'
 ADDRESS = "Missouri University of Science and Technology"
 PLACE_ID_STARTSWITH = "ChIJr3S"
 
-urlParameters = dict()
-urlParameters['address'] = ADDRESS
-urlParameters['key'] = API_KEY
-
 # Ignore SSL certificate errors
 # See previous notes about this SSL context 
 sslContext = ssl.create_default_context()
@@ -21,33 +17,37 @@ sslContext.check_hostname = False
 sslContext.verify_mode = ssl.CERT_NONE
 
 # Use urllib to convert a py dictionary into a CGI URL
-# address=South+Federal+University&key=42
+# For example: address=South+Federal+University&key=42
+# urllib.parse.urlencode requires a dictionary as input
+urlParameters = dict()
+urlParameters['address'] = ADDRESS
+urlParameters['key'] = API_KEY
 url = SERVICEURL + urllib.parse.urlencode(urlParameters)
 
 print('Retrieving', url)
-# see previous notes about usign urllib for HTTP GET & responses
+# see previous notes about using urllib for HTTP GET & responses
 urllibHandle = urllib.request.urlopen(url, context=sslContext)
 data = urllibHandle.read().decode()
 print('Retrieved', len(data), 'characters')
 
 try:
-    # Convert JSON string into py dict or list
+    # Convert JSON string into py dict/list
     jsonDict = json.loads(data)
-except:
+except NameError:
     jsonDict = None
 
 if not jsonDict or 'status' not in jsonDict or jsonDict['status'] != 'OK':
     print('==== Failure To Retrieve ====')
     print(data)
 
-# debug     print(json.dumps(js, indent=4))
-# debug junk = input("CTL-c to quit, RTN to continue")
-
-# Used "text" folding in Vim to find where place_id was in the large JSON file
+# TIP: Open the retrieved JSON file in Vim and use text folding to determine how to reference 'place_id'
+# In Vim
 # :set filetype=json
 # :syntax on
 # :set foldmethod=syntax
 # zo, zc & za (while cursor is on fold point)
-#
 print("Place id:", jsonDict['results'][0]['place_id'])
-# print( "place_id_startswith:", place_id_startswith )
+# debug print( "place_id_startswith:", place_id_startswith )
+
+# debug     print(json.dumps(js, indent=4))
+# debug junk = input("CTL-c to quit, RTN to continue")
