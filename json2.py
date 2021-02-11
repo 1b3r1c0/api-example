2fd75497e18bfd6876c9786b7731c7e070699f3e
1b3r1c0 <baconandeggs@pm.me>
2021-02-11 09:48:58 -0500

    cleaned up per pep 8, added comments

--- json2.py
+++ json2.py
@@ -1,46 +1,37 @@
 #!/bin/python3
 
-import urllib.request, urllib.parse, urllib.error
+import urllib.request
+import urllib.error
 import json
 import ssl
+# I don't think this module is used
+# import urllib.parse
 
-#-----------------------------
-# Retrieve data from a web page
+# Define Constants
+# URL to retrieve data from
+# Sample: URL = "http://py4e-data.dr-chuck.net/comments_42.json"
+URL = "http://py4e-data.dr-chuck.net/comments_1123611.json"
 
-# Create an SSL "contex" for urllib
+# Create an SSL "context" for urllib
 # This will Ignore SSL certificate errors
-ctx = ssl.create_default_context() 
-ctx.check_hostname = False 
-ctx.verify_mode = ssl.CERT_NONE 
-
-# URL to retrieve data from 
-# Sample
-# url = "http://py4e-data.dr-chuck.net/comments_42.json"
-# graded
-url = "http://py4e-data.dr-chuck.net/comments_1123611.json"
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
 
 # Make an HTTP connection and make the request 
-uh = urllib.request.urlopen(url, context=ctx) 
+uh = urllib.request.urlopen(URL, context=ctx)
 
-# put the response into a varible 
+# put the response into a variable
 data = uh.read()
 
-# debug 
-# convert from a "byte" string, or more likely a UTF-8 string, into a unicode string 
-# data.decode() 
-
-#----------------------------- 
-# Extract JSON data 
-
 # The json library Converts raw JSON into a python dictionary of nested lists & dictionaries
-# It doesn't convert the raw data into an object like the etree lib does for XML
+# It doesn't convert the raw data into an object like xml.etree does for XML
 info = json.loads(data)
-# The data structure will be somewhat complex, for example:
-# nested dictionaeries in nested lists in a dictionary
+# The data structure will be something like nested dictionaries in nested lists in a dictionary
 
 # The value corresponding to the 'comments' key is a list that's iterated over
 # each element of that list is a nested dictionary
 # We want the "count: <number>" key/value pair from that nested dictionary
-print( sum( [ x['count'] for x in info['comments'] ] ) )
-#    debug = input("CTL+c to quit, RTN to continue") 
-
+# List comprehension
+print(sum([x['count'] for x in info['comments']]))
+#    debug = input("CTL+c to quit, RTN to continue")
